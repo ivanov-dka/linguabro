19892ab193e7a91004db61ff19e316275f811505
treponat <treponat@gmail.com>
2017-09-28 23:01:01 +0300

    сабы, первый вариант

---
+++ LinguaBroAuth/Scripts/SubtitilesWork.js
@@ -0,0 +1,22 @@
+﻿var subData;
+var subText;
+
+
+function initSubCreation(subText) {
+
+    subData = parseSRT(subText);
+}
+
+function createSub(second) {
+    for (var i = 0; i < subData.length; i++) {
+        if (second < subData[i].end && second > subData[i].start) {
+            var subText = subData[i].text.match(/[^ ]+/g)
+            $('#subs').html('');
+            for (var i = 0; i < subText.length; i++) {
+                $('#subs').append('<a href="#">' + ' ' + subText[i] + ' '  + '</a>')
+            }
+            return;
+        }
+    }
+    $('#subs').html('');
+}
