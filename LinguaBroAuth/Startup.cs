452153739e66ad62993709349b006730b5b4e671
Ivanov Vladimir <ivanov@k2it.ru>
2017-09-28 16:28:18 +0300

    SignalR added

---
+++ LinguaBroAuth/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinguaBroAuth.Startup))]
+namespace LinguaBroAuth
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.MapSignalR();
+            ConfigureAuth(app);            
+        }
+    }
+}
